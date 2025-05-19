75825fee2e907b872ebb57e78e130bf599023f78
soufianeoualla <soufianeoualla0@gmail.com>
2025-05-19 12:07:54 +0100

    adjust navigation

--- src/components/ui/CustomListItem.tsx
+++ src/components/ui/CustomListItem.tsx
@@ -1,7 +1,7 @@
 export default function CustomListItem({ label }: { label: string }) {
   return (
-    <li className="group h-[32px] p-2 overflow-hidden cursor-pointer">
-      <div className="flex flex-col items-center justify-center group-hover:-translate-y-7.5 group-hover:font-medium transition duration-700">
+    <li className="group h-[30px] p-2 overflow-hidden cursor-pointer">
+      <div className="flex flex-col  items-center justify-center group-hover:-translate-y-7.5 group-hover:font-medium transition duration-700">
         <span>{label}</span>
         <span>{label}</span>
       </div>
